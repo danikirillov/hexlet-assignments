4719e6d947af120f381fa66f83927cb0cb7d230d
@hexlet/cli <support@hexlet.io>
2025-03-21 00:26:51 +0100

    submit java-spring-boot-ru/response-entity

--- java-spring-boot-ru/response-entity/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/response-entity/src/main/java/exercise/Application.java
@@ -1,13 +1,10 @@
 package exercise;
 
-import java.net.URI;
-import java.util.List;
-import java.util.Optional;
-
+import exercise.model.Post;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
 
-import exercise.model.Post;
+import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
