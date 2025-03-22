4d47755cf38a06ff7e0f662e674346ccd972d2c3
@hexlet/cli <support@hexlet.io>
2025-03-22 21:39:16 +0100

    submit java-spring-boot-ru/jpa-data

---
+++ java-spring-boot-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
@@ -0,0 +1,59 @@
+package exercise.controller;
+
+import exercise.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+import exercise.model.Person;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/people")
+public class PeopleController {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @GetMapping(path = "/{id}")
+    public Person show(@PathVariable long id) {
+        return personRepository.findById(id).get();
+    }
+
+    // BEGIN
+    @GetMapping("/people")
+    @ResponseStatus(OK)
+    public List<Person> getAll() {
+        return personRepository.findAll();
+    }
+
+    @PostMapping("/posts")
+    @ResponseStatus(CREATED)
+    public Person create(@RequestBody Person person) {
+        return personRepository.save(person);
+    }
+
+    @DeleteMapping("/posts/{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void destroy(@PathVariable String id) {
+        personRepository.deleteById(Long.valueOf(id));
+    }
+    // END
+}
